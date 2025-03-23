3554c1bab00166facfa5d97405f7e8da5ae4ab7a
Drew Krehel <drewkrehel@gmail.com>
2025-03-23 21:04:47 +0000

    starting

--- app.rb
+++ app.rb
@@ -8,3 +8,21 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("rock") do
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_moves = moves.sample
+  
+  if comp_moves == "rock"
+    @outcome = "tied"
+  elsif comp_moves == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:rock)
+end
+
+ 
