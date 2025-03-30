d2b0ed1931b2681a4dbaa8b002e2bbb6ac128c7f
Drew Krehel <drewkrehel@gmail.com>
2025-03-30 16:06:02 +0000

    dynamic changes

--- app.rb
+++ app.rb
@@ -6,50 +6,71 @@ get("/") do
   erb(:homepage)
 end
 
-get("/rock") do
-  moves = ["rock", "paper", "scissors"]
+# get("/rock") do
+#   moves = ["rock", "paper", "scissors"]
   
-  @comp_moves = moves.sample
+#   @comp_moves = moves.sample
   
-  if @comp_moves == "rock"
-    @outcome = "tied"
-  elsif @comp_moves == "paper"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-  end
+#   if @comp_moves == "rock"
+#     @outcome = "tied"
+#   elsif @comp_moves == "paper"
+#     @outcome = "lost"
+#   else
+#     @outcome = "won"
+#   end
   
-  erb(:rock)
-end
+#   erb(:rock)
+# end
 
-get("/paper") do
-  moves = ["rock", "paper", "scissors"]
+# get("/paper") do
+#   moves = ["rock", "paper", "scissors"]
   
-  @comp_moves = moves.sample
+#   @comp_moves = moves.sample
   
-  if @comp_moves == "paper"
-    @outcome = "tied"
-  elsif @comp_moves == "scissors"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-  end
+#   if @comp_moves == "paper"
+#     @outcome = "tied"
+#   elsif @comp_moves == "scissors"
+#     @outcome = "lost"
+#   else
+#     @outcome = "won"
+#   end
   
-  erb(:paper)
-end
+#   erb(:paper)
+# end
 
-get("/scissors") do
-  moves = ["rock", "paper", "scissors"]
+# get("/scissors") do
+#   moves = ["rock", "paper", "scissors"]
   
-  @comp_moves = moves.sample
+#   @comp_moves = moves.sample
   
-  if @comp_moves == "scissors"
+#   if @comp_moves == "scissors"
+#     @outcome = "tied"
+#   elsif @comp_moves == "rock"
+#     @outcome = "lost"
+#   else
+#     @outcome = "won"
+#   end
+  
+#   erb(:scissors)
+# end
+
+get("/:hand") do
+  @moves = ["rock", "paper", "scissors"]
+  @comp_moves = @moves.sample
+  
+  @player_moves = params.fetch("hand")
+
+  if @comp_moves == @player_moves
     @outcome = "tied"
-  elsif @comp_moves == "rock"
+  elsif @comp_moves == "rock" && @player_moves == "scissors"
+    @outcome = "lost"
+  elsif @comp_moves == "paper" && @player_moves == "rock"
+    @outcome = "lost"
+  elsif @comp_moves == "scissors" && @player_moves == "paper"
     @outcome = "lost"
   else
     @outcome = "won"
   end
   
-  erb(:scissors)
+  erb(:flexible)
 end
